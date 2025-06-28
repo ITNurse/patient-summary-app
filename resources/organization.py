58f4b492ede1af2ef079d45ba8bf7fc611a66831
ITNurse <lisa.totton@gmail.com>
2025-06-28 17:42:31 -0300

    Converted to version that leverages fhir.resources python library

--- resources/organization.py
+++ resources/organization.py
@@ -1,17 +1,17 @@
+from fhir.resources.organization import Organization
 from config import ORGANIZATION_ID, ORGANIZATION_NAME
 
 
 def create_organization_resource():
     """
-    Create a FHIR Organization resource.
-    
+    Create a FHIR Organization resource using fhir.resources.
+
     Returns:
-        tuple: (organization_id, organization_resource)
+        tuple: (organization_id, organization_resource_dict)
     """
-    organization_resource = {
-        "resourceType": "Organization",
-        "id": ORGANIZATION_ID,
-        "name": ORGANIZATION_NAME
-    }
-    
-    return ORGANIZATION_ID, organization_resource
+    organization = Organization(
+        id=ORGANIZATION_ID,
+        name=ORGANIZATION_NAME
+    )
+
+    return ORGANIZATION_ID, organization.dict(by_alias=True)
